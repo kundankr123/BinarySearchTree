2a69d633c14fa25013e6e5589fc640d1f7a471b4
kundan <goswamikundan123@gmail.com>
2023-02-27 18:25:19 +0530

    Adding files which creates a BST and displays the data using Preorder Traversal.

---
+++ BinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreePrograms
+{
+    public class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+
+                while (true)
+                {
+                    parent = current;
+
+                    if (data < current.data)
+                    {
+                        current = current.left;
+
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PreorderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                Console.WriteLine(node.data+" ");
+                PreorderTraversal(node.left);
+                PreorderTraversal(node.right);
+            }
+        }
+    }
+}
+    
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to Master branch of Binary Search Tree.");        
+            //Console.WriteLine("Welcome to Master branch of Binary Search Tree.");
+
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+
+            Console.WriteLine("Preorder Traversal of BST:");
+            bst.PreorderTraversal(bst.root);
         }
 
     }
