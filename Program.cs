c0c0a9aef77f43c3f0b1c4496dcaf76c04ac34ae
kundan <goswamikundan123@gmail.com>
2023-02-27 17:05:37 +0530

    Adding file which displays the greeting message.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BinarySearchTreePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Master branch of Binary Search Tree.");        
+        }
+
+    }
+}
